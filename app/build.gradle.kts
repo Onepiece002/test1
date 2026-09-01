643c492412350ab3d1b521423a263111e352dc61
Nrajesh0 <superk2537@gmail.com>
2026-09-01 12:33:56 +0530

    33

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
     multiDexEnabled = true
     minSdk = 24
     targetSdk = 35
-    versionCode = 32
-    versionName = "1.3.2"
+    versionCode = 33
+    versionName = "1.3.3"
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
 
